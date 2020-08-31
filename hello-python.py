b5bda818ba6be459fbb453dd39056efaa70a523c
Govindarajan V <govindarajanv@outlook.com>
2020-08-31 13:49:02 +0530

    Update hello-python.py

--- hello-python.py
+++ hello-python.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 os.system("clear")
-print ("You are using Python from path",sys.version)
-print ("You are using Python version",sys.executable)
+print("You are using Python from path", sys.version)
+print("You are using Python version", sys.executable)
